631b573d057f436a123cb1148e7fc4931bf65337
Ali Ganji <aliganji142879@gmail.com>
2024-06-22 18:50:50 +0330

    change forms customuser

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,15 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.views import generic
+from django.urls import reverse_lazy
 
-from .models import CustomUser
+from .forms import CustomUserCreationForm
 
 
-class SignUpView(UserCreationForm):
-    pass
+class SignUpView(generic.CreateView):
+    form_class = CustomUserCreationForm
+    template_name = 'registration/signup.html'
+    success_url = reverse_lazy('login')
+
+
+
+
+
