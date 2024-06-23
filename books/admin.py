f358028878ad0f87561bd5c3759a8257a8dc8255
Ali Ganji <aliganji142879@gmail.com>
2024-06-23 20:31:27 +0330

    add comment form to book detail view

--- books/admin.py
+++ books/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from .models import Book
+from .models import Book, Comment
 
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'price', 'datetime_created', 'datetime_modified')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('book', 'user', 'text', 'recommend', 'datetime_created', 'is_active',)
