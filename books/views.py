1dcbb071f226f18d3498a21ca054948e3c093b78
Ali Ganji <aliganji142879@gmail.com>
2024-06-24 11:51:22 +0330

    add login required to views

--- books/views.py
+++ books/views.py
@@ -1,7 +1,8 @@
 from django.views import generic
 from django.urls import reverse_lazy
 from django.shortcuts import get_object_or_404, render
-
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
 from .models import Book, Comment
 from .forms import CommentForm
 
@@ -13,6 +14,7 @@ class BookListView(generic.ListView):
     context_object_name = 'books'
 
 
+@login_required()
 def book_detail_view(request, pk):
     book = get_object_or_404(Book, pk=pk)
     book_comments = book.comments.all()
@@ -33,19 +35,19 @@ def book_detail_view(request, pk):
         'comment_form': comment_form})
 
 
-class BookCreateView(generic.CreateView):
+class BookCreateView(LoginRequiredMixin, generic.CreateView):
     model = Book
     fields = ['title', 'author', 'description', 'price', 'cover']
     template_name = 'books/book_create.html'
 
 
-class BookUpdateView(generic.UpdateView):
+class BookUpdateView(LoginRequiredMixin, generic.UpdateView):
     model = Book
     fields = ['title', 'author', 'description', 'price', 'cover']
     template_name = 'books/book_update.html'
 
 
-class BookDeleteView(generic.DeleteView):
+class BookDeleteView(LoginRequiredMixin, generic.DeleteView):
     model = Book
     template_name = 'books/book_delete.html'
     success_url = reverse_lazy('book_list')
